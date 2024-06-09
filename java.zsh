a467508d25fd399c9bd02a7c7f7bd69b23f55ee8
Pavel Likin <pavellikin@gmail.com>
2024-06-09 17:45:50 +0200

    Add .psqlrc and update java.

--- java.zsh
+++ java.zsh
@@ -1,2 +1,3 @@
-alias setjdk8='export JAVA_HOME=$(/usr/libexec/java_home -v 1.8); export PATH=$JAVA_HOME/bin:$PATH'
 alias setjdk11='export JAVA_HOME=$(/usr/libexec/java_home -v 11); export PATH=$JAVA_HOME/bin:$PATH'
+alias setjdk17='export JAVA_HOME=$(/usr/libexec/java_home -v 17); export PATH=$JAVA_HOME/bin:$PATH'
+alias setjdk21='export JAVA_HOME=$(/usr/libexec/java_home -v 21); export PATH=$JAVA_HOME/bin:$PATH'
