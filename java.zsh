3c48fad166a39074a929095e755648381a814199
Pavel Likin <pavellikin@gmail.com>
2026-03-21 14:15:04 +0100

    Update java.zsh, remove wezterm config

--- java.zsh
+++ java.zsh
@@ -1,3 +1,2 @@
-alias setjdk11='export JAVA_HOME=$(/usr/libexec/java_home -v 11); export PATH=$JAVA_HOME/bin:$PATH'
-alias setjdk17='export JAVA_HOME=$(/usr/libexec/java_home -v 17); export PATH=$JAVA_HOME/bin:$PATH'
 alias setjdk21='export JAVA_HOME=$(/usr/libexec/java_home -v 21); export PATH=$JAVA_HOME/bin:$PATH'
+alias setjdk21='export JAVA_HOME=$(/usr/libexec/java_home -v 25); export PATH=$JAVA_HOME/bin:$PATH'
